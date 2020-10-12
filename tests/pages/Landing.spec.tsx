f1ca3794a9a99239fd2bf588c4e26768dcd19c3d
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-12 20:52:07 -0300

    Test Landing

---
+++ tests/pages/Landing.spec.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+import { Router } from 'react-router-dom';
+
+import Landing from '../../src/pages/Landing';
+
+describe('Landing page', () => {
+  it('should be able to navigate to map page', () => {
+    const history = createMemoryHistory();
+    const { getByTestId } = render(
+      <Router history={history}>
+        <Landing />
+      </Router>,
+    );
+
+    const button = getByTestId('map');
+    expect(button).toBeInTheDocument();
+
+    fireEvent.click(button);
+
+    expect(history.location.pathname).toBe('/app');
+  });
+});
