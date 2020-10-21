1bbee7afc3e012b0db826f0f43c80b0fd46a0857
DiegoVictor <diegovictorgonzaga@gmail.com>
2020-10-21 20:02:49 -0300

    Test creation orphanage page

---
+++ tests/pages/CreateOrphanage.spec.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import { act, fireEvent, render } from '@testing-library/react';
+import MockAdapter from 'axios-mock-adapter';
+import faker from 'faker';
+import { toast } from 'react-toastify';
+
+import CreateOrphanage from '../../src/pages/CreateOrphanage';
+import factory from '../utils/factory';
+import api from '../../src/services/api';
+
+interface Orphanage {
+  name: string;
+  about: string;
+  instructions: string;
+  opening_hours: string;
+  whatsapp: string;
+}
+
+const mockedPush = jest.fn(() => {});
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: () => ({
+      goBack: () => {},
+      push: mockedPush,
+    }),
+  };
+});
+
+jest.mock('react-toastify');
+
+const imageUrl = faker.image.imageUrl();
+global.URL.createObjectURL = jest.fn(() => {
+  return imageUrl;
+});
+
+describe('CreateOrphanage', () => {
+  const apiMock = new MockAdapter(api);
+
+  it('should not be able to create an orphanage', async () => {
+    const orphanage = await factory.attrs<Orphanage>('Orphanage');
+
+    apiMock.onPost('/orphanages').reply(404, {});
+
+    const { getByTestId, getByLabelText, container } = render(
+      <CreateOrphanage />,
+    );
+
+    await act(async () => {
+      fireEvent.change(getByLabelText('Nome'), {
+        target: { value: orphanage.name },
+      });
+      fireEvent.change(getByTestId('about'), {
+        target: { value: orphanage.about },
+      });
+      fireEvent.change(getByLabelText('Instruções'), {
+        target: { value: orphanage.instructions },
+      });
+      fireEvent.change(getByLabelText('Horário de functionamento'), {
+        target: { value: orphanage.opening_hours },
+      });
+      fireEvent.change(getByLabelText('WhatsApp'), {
+        target: { value: orphanage.whatsapp },
+      });
+      fireEvent.change(getByTestId('add_image'), {
+        target: {},
+      });
+
+      fireEvent.click(container.querySelector('.leaflet-container'));
+    });
+
+    await act(async () => {
+      fireEvent.click(getByTestId('submit'));
+    });
+
+    expect(mockedPush).not.toHaveBeenCalledWith('/app');
+    expect(global.URL.createObjectURL).not.toHaveBeenCalledWith(new Blob());
+    expect(toast.error).toHaveBeenCalledWith(
+      'Ops! Alguma coisa deu errada tente novamente',
+    );
+  });
+
+  it('should be able to create an orphanage', async () => {
+    const orphanage = await factory.attrs<Orphanage>('Orphanage');
+    apiMock.onPost('/orphanages').reply(200);
+
+    const { getByTestId, getByLabelText, container } = render(
+      <CreateOrphanage />,
+    );
+
+    await act(async () => {
+      fireEvent.change(getByLabelText('Nome'), {
+        target: { value: orphanage.name },
+      });
+      fireEvent.change(getByTestId('about'), {
+        target: { value: orphanage.about },
+      });
+      fireEvent.change(getByLabelText('Instruções'), {
+        target: { value: orphanage.instructions },
+      });
+      fireEvent.change(getByLabelText('Horário de functionamento'), {
+        target: { value: orphanage.opening_hours },
+      });
+      fireEvent.change(getByLabelText('WhatsApp'), {
+        target: { value: orphanage.whatsapp },
+      });
+
+      fireEvent.click(container.querySelector('.leaflet-container'));
+      fireEvent.click(getByTestId('open_on_weekends'));
+      fireEvent.click(getByTestId('not_open_on_weekends'));
+      fireEvent.change(getByTestId('add_image'), {
+        target: {
+          files: [new Blob(['image'])],
+        },
+      });
+    });
+
+    expect(getByTestId('preview_0')).toHaveAttribute('src', imageUrl);
+
+    await act(async () => {
+      fireEvent.click(getByTestId('submit'));
+    });
+
+    expect(mockedPush).toHaveBeenCalledWith('/app');
+    expect(global.URL.createObjectURL).toHaveBeenCalledWith(new Blob());
+  });
+
+  it('should not be able to create an orphanage with invalid data', async () => {
+    apiMock.onPost('/orphanages').reply(200);
+
+    const { getByTestId, getAllByText, getByText } = render(
+      <CreateOrphanage />,
+    );
+
+    await act(async () => {
+      fireEvent.click(getByTestId('submit'));
+    });
+
+    expect(mockedPush).toHaveBeenCalledWith('/app');
+    expect(global.URL.createObjectURL).toHaveBeenCalledWith(new Blob());
+
+    expect(getAllByText('Este campo é obrigatório').length).toBe(5);
+    expect(getByText('Marque um ponto no mapa')).toBeInTheDocument();
+  });
+});
