7c41050155fe9e24d4dfa923a35f92bd091c885f
Diego Victor <diegovictorgonzaga@gmail.com>
2022-02-08 20:07:03 -0300

    Update dependencies

--- tests/utils/factory.ts
+++ tests/utils/factory.ts
@@ -5,22 +5,22 @@ factory.define(
   'Orphanage',
   {},
   {
-    id: () => String(faker.random.number()),
+    id: () => String(faker.datatype.number()),
     name: faker.name.title,
     about: faker.lorem.sentence,
     latitude: faker.address.latitude,
     longitude: faker.address.longitude,
     instructions: faker.lorem.sentence,
     opening_hours: faker.lorem.sentence,
-    open_on_weekends: faker.random.boolean,
+    open_on_weekends: faker.datatype.boolean,
     whatsapp: faker.phone.phoneNumber,
     images: [
       {
-        id: faker.random.number,
+        id: faker.datatype.number,
         path: faker.image.imageUrl,
       },
       {
-        id: faker.random.number,
+        id: faker.datatype.number,
         path: faker.image.imageUrl,
       },
     ],
