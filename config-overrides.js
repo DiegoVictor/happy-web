833f3fa7fb4f0f6440d5eabc6d27ffe06d36e635
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-11-26 11:17:25 -0300

    Add coveragePathIgnorePatterns to Jest configuration for improved test coverage management

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ const jestConfig = require('./jest.config');
 module.exports = {
   jest(config) {
     config.preset = jestConfig.preset;
+    config.coveragePathIgnorePatterns = jestConfig.coveragePathIgnorePatterns;
     config.reporters = jestConfig.reporters;
     return config;
   },
--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,10 @@
 module.exports = {
+  coveragePathIgnorePatterns: [
+    '/src/routes/',
+    '/src/services/',
+    '/src/styles/',
+    '/src/App.tsx',
+    '/src/index.tsx',
+  ],
   preset: 'ts-jest',
 };
