b47119ce4a58ae11f91b74fe204ffa0dc3e909f2
DiegoVictor <diegovictorgonzaga@gmail.com>
2025-11-27 11:02:23 -0300

    Refactor Landing tests to use MemoryRouter and improve navigation handling

--- tests/integration/Landing.spec.tsx
+++ tests/integration/Landing.spec.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { fireEvent, render } from '@testing-library/react';
 import { createMemoryHistory } from 'history';
-import { Router } from 'react-router-dom';
+import { MemoryRouter, Route, Router, Routes } from 'react-router-dom';
 
 import Landing from '../../src/pages/Landing';
 
 describe('Landing page', () => {
   it('should be able to navigate to map page', () => {
-    const history = createMemoryHistory();
-    const { getByTestId } = render(
-      <Router location={history.location} navigator={history}>
-        <Landing />
-      </Router>,
+    const pageTitle = 'Orphanages Map Page';
+    const { getByTestId, getByText } = render(
+      <MemoryRouter
+        initialEntries={['/']}
+        future={{
+          v7_relativeSplatPath: true,
+          v7_startTransition: true,
+        }}
+      >
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/app" element={<div>{pageTitle}</div>} />
+        </Routes>
+      </MemoryRouter>,
     );
 
     const button = getByTestId('map');
@@ -19,6 +28,6 @@ describe('Landing page', () => {
 
     fireEvent.click(button);
 
-    expect(history.location.pathname).toBe('/app');
+    expect(getByText(pageTitle)).toBeInTheDocument();
   });
 });
